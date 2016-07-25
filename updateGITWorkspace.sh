5bc7dbc2efde896c77a1d16ca5e333be7794c3fe
Neeraj Kumar <kumarneraj@gmail.com>
2016-07-25 11:28:39 -0700

    Updating to check for all git repos under current directory using find

--- updateGITWorkspace.sh
+++ updateGITWorkspace.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
+
 ############################################################################
 ## This script is used for updating all the GIT Repo under the executing directory path
 ## For Example: myfolder +-----> updateGITWorkspace.sh
-##                       |-----> Git_Repo_2
-##                       +-----> Git_Repo_1
+##                       |-----> Git_Repo_1
+##                       +-----> Git_Repo_2
+##                       +-----> DIR1/Git_Repo_3
+##                       +-----> DIR2/DIR3/Git_Repo_4
+##                       +-----> DIR2/DIR3/Git_Repo_5
+##
 ##
 ## In this case, if executed from my folder path, it will update both of the 
 ##   GIT repositories: Git_Repo_2 and Git_Repo_1. Its just finding Git repos 
 ##   under executing directory path and then git pull.
 ############################################################################
 
-
-file=".git"
-
-DIRS=`ls -l | egrep '^d' | awk '{print $NF}'`
+ORG_DIR=`pwd`
+DIRS=`find . -type d -name .git | awk -F".git" '{print $1}'`
 for DIR in $DIRS
 do
-if [ -e ${DIR}/$file ]
-then
+
     echo ""
-    echo "Updating local git repo from remote, for folders:"
-    echo "#############"
+    echo "## Updating local git repo from remote, for folders:"
+    echo "##-----------"
     echo "## " ${DIR}
-    echo "#############"
+    echo "##-----------"
 
     cd ${DIR}
     git pull
-    cd ..
+    cd ${ORG_DIR}
 
-    echo "#############"
+    echo "##-----------"
     echo ""
-fi;
 
 done
